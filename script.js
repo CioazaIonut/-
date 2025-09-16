c0844065a44332ba4e0dd22cff09a6b22aabc097
CioazaIoan <cioazai@yahoo.com>
2025-09-16 18:45:40 +0300

    unu

---
+++ script.js
@@ -0,0 +1,35 @@
+// Compară două numere diferite:
+
+let a = 5;
+let b = 10;
+console.log(a !== b);
+
+// Compară două string-uri diferite:
+
+let string1 = "hello";
+let string2 = "world";
+console.log(string1 !== string2);
+
+// Compară număr și string cu aceeași valoare:
+
+let num = 7;
+let str = "7";
+console.log(num !== str);
+
+// Compară două valori boolean:
+
+let x = true;
+let y = false;
+console.log(x !== y);
+
+// Compară null și undefined:
+
+let c = null;
+let d = undefined;
+console.log(c !== d);
+
+// Compară array-uri:
+
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3];
+console.log(arr1 !== arr2);
